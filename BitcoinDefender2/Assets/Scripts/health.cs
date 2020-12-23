8ce3ca5705cc622cb967358b4e70ecd92fc43d40
SaltMeister <huisimonliu@gmail.com>
2020-12-22 23:55:29 -0800

    attempted making hp script for the wall

---
+++ BitcoinDefender2/Assets/Scripts/health.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class health : MonoBehaviour
+{
+	private int hp;
+	public Text hpText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        hp = 100;
+        hpText.text = hp.ToString();
+        Debug.Log(hp);
+        Debug.Log(hpText);
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+          //TakeDamage(10);
+        }
+    }
+}
+/*
+    void TakeDamage(int dmg)
+    {
+    	hp = dmg;
+    	hpText.text = hp.toString();
+    }
+*/
---
+++ BitcoinDefender2/Assets/healthBar.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class healthBar : MonoBehaviour
+{
+
+}
+/*
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    	private int hp;
+		public Text hpText;
+        hp = 100;
+        hpText.text = hp.toString();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+          TakeDamage(10);
+        }
+    }
+    void TakeDamage(int dmg)
+    {
+    	hp = dmg;
+    	hpText.text = hp.toString();
+    }
+*/
